293313b1080fa64d81afa5924564530aad71e9f9
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-05-31 14:31:08 +0400

    Added eslint and data fetching from api

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Original video: [https://www.youtube.com/watch?v=m-MAIpnH9ag](https://www.youtub
 
 Original code repo: [https://github.com/bradtraversy/vue-covid-tracker](https://github.com/bradtraversy/vue-covid-tracker)
 
+[Day.js](https://github.com/iamkun/dayjs) used for Moment.js alternative.
+
 Created with ⚡ [Vite](https://vitejs.dev/)
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,52 @@
 <template>
-  <div class="flex flex-col">
-    <p>Hello from Vue 3</p>
+  <div class="container">
+    <Header />
+    <main v-if="!loading">
+      show data
+    </main>
+    <main v-else class="flex flex-col align-center justify-center text-center">
+      <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching data...</div>
+      <img :src="loadingImage" class="w-24 m-auto">
+    </main>
   </div>
 </template>
 
-<script setup>
+<script>
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+import Header from "./components/Header.vue";
+import Hourglass from './assets/hourglass.gif'
+
+export default {
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: Hourglass
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    //console.log(data);
+    this.dataDate = data.dataDate
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+
+  },
+};
+</script>
